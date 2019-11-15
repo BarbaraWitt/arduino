ad9fbddfee6c3e549ee31a86558b598731e68d7a
BarbaraWitt <57507915+BarbaraWitt@users.noreply.github.com>
2019-11-14 22:29:53 -0300

    Add files via upload

---
+++ aula4/estudoWhile/estudoWhile.ino
@@ -0,0 +1,16 @@
+/**
+   Estudo do laço while / do while
+   @author José de Assis
+*/
+
+void setup() {
+  Serial.begin(9600);
+  int x = 2;
+  do {
+    Serial.println("Hello");
+  } while (x != 2);
+}
+
+void loop() {
+
+}
